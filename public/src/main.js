302cf2c57213fa443c21566d3199f4c05e92fd79
Seth Berrier <berriers@uwstout.edu>
2019-02-12 10:47:01 -0600

    Starting code for the tutorials - Almost identical to the official 'hello world' example - see Phaser.io's getting started section

--- public/src/main.js
+++ public/src/main.js
@@ -1,48 +1,58 @@
-/* global GameScene */
-
-// Configure any plugins
-let scenePlugins = [{
-  key: 'updatePlugin',
-  plugin: Phaser.Plugins.UpdatePlugin,
-  mapping: 'updates'
-}]
-
-// Add the debugDraw plugin if in DEV mode
-if (__DEV__) {
-  scenePlugins.push({
-    key: 'DebugDrawPlugin',
-    plugin: PhaserDebugDrawPlugin,
-    mapping: 'debugDraw'
-  })
-}
-
-// Phaser base configuration
+// Core phaser configuration object
 let config = {
-  type: Phaser.WEBGL,
-  scale: {
-    parent: 'content',
-    mode: Phaser.Scale.FIT,
-    autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-    width: window.CONFIG.gameWidth,
-    height: window.CONFIG.gameHeight
-  },
-  title: 'UWStout GDD325 Phaser Lite',
-  backgroundColor: '#7f7f7f',
-  plugins: {
-    scene: scenePlugins
-  },
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
   physics: {
     default: 'arcade',
-    debug: __DEV__,
     arcade: {
       gravity: { y: 200 }
     }
+  },
+  scene: {
+    preload: preload,
+    create: create
   }
 }
 
-// Create the primary game object and attach to the global 'window' object
+// Start Phaser by making a Phaer.Game object and passing in the
+// config object from above.  We attach this to 'window' so it will
+// be globally accessible.
 window.game = new Phaser.Game(config)
 
-// Register and start the main scene
-window.game.scene.add('MainScene', GameScene, false)
-window.game.scene.start('MainScene')
+// Queue up assets to load from our local assets directory
+// Note: these are loaded asyncronously after preload() completes
+// and before 'create()' is run.
+function preload () {
+  this.load.image('sky', 'assets/skies/space3.png')
+  this.load.image('logo', 'assets/sprites/phaser3-logo.png')
+  this.load.image('red', 'assets/particles/red.png')
+}
+
+// Build the scene by adding GameObjects and configuring specific
+// entities (runs after all queued assets are loaded)
+function create () {
+  // The background sky
+  this.add.image(400, 300, 'sky')
+
+  // A sprite to use as a particle
+  let particles = this.add.particles('red')
+
+  // A particle emitter
+  let emitter = particles.createEmitter({
+    speed: 100,
+    scale: { start: 1, end: 0 },
+    blendMode: 'ADD'
+  })
+
+  // The Phaser 3 logo (which sill bounce around the screen)
+  let logo = this.physics.add.image(400, 100, 'logo')
+
+  // Make the logo move
+  logo.setVelocity(100, 200)
+  logo.setBounce(1, 1)
+  logo.setCollideWorldBounds(true)
+
+  // Make the emitter follow the logo
+  emitter.startFollow(logo)
+}
