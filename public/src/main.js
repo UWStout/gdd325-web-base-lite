136c4219d743ab68d32881a5e0adc8c766343cc3
Seth Berrier <berriers@uwstout.edu>
2019-02-12 11:32:41 -0600

    Comments in main

--- public/src/main.js
+++ public/src/main.js
@@ -13,9 +13,10 @@ let config = {
   }
 }
 
-// Start Phaser by making a Phaer.Game object and passing in the
+// Start Phaser by making a Phaser.Game object and passing in the
 // config object from above.  We attach this to 'window' so it will
 // be globally accessible.
 window.game = new Phaser.Game(config)
 
+// Tell it how to make a GameScene object and let it automatically start
 window.game.scene.add('main', GameScene, true)
